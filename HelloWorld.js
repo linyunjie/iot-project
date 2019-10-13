13f0b01b952121b22a6eefe34ef67b586ae0d150
linyujie <jinne19952001@gmail.com>
2019-10-14 04:30:13 +0800

    'test'

--- HelloWorld.js
+++ HelloWorld.js
@@ -5,7 +5,7 @@ const http = require('http');
 //const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
 
 //port 號會由 Heroku 給予，因此不再自行指定
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 //新增一個server並指定他的頁面資訊，內容為'Hello World!'
 const server = http.createServer((req, res) => {
@@ -15,4 +15,4 @@ const server = http.createServer((req, res) => {
 });
 
 //監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Listening on ${PORT}`));
+//server.listen(port, () => console.log(`Listening on ${PORT}`));
